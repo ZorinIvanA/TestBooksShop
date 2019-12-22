452137f16d03e5f838f3187cbd4121d04361d814
ZorinIvanA <zorin.ivan@outlook.com>
2019-12-22 18:52:19 +0300

    База данных

--- Scripts/books_common_books.sql
+++ Scripts/books_common_books.sql
@@ -1,5 +1,5 @@
 INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (8, 'Словарь Даля', 'В.И. Даль', 25, 3, 45);
-INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (3, 'Властелин колец', 'Толкин', 30, 2, 119);
-INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (7, 'Тувия - дева Марса', 'Э. Р. Берроуз', 34, 1, 114);
-INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (1, 'Большая Советская энциклопедия', 'Много кто', 100, 4, 95);
-INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (2, 'Будет скафандр, будет и путешествие', 'Р. Хайнлайн', 40, 1, 48);
+INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (2, 'Психиатрия. Учебное пособие для студентов медицинских вузов', 'Самохвалов В.П.', 40, 1, 47);
+INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (1, 'Большая Советская энциклопедия', 'Шмидт О.Ю.', 100, 4, 95);
+INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (3, 'Физика для чайников', 'Хольцнер С.', 30, 2, 119);
+INSERT INTO common.books (id, name, author, price, type, present_on_storage) VALUES (7, 'Физика. Квантовая физика.', 'Тюрин Ю.И.', 34, 1, 114);
